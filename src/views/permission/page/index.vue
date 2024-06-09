95e6c2508859f0c57ab13cbff197458654e56dad
Cade Li <cadecode@foxmail.com>
2024-06-09 18:15:45 +0800

    refactor: 页面权限去掉切换用户功能

--- src/views/permission/page/index.vue
+++ src/views/permission/page/index.vue
@@ -1,9 +1,6 @@
 <script setup lang="ts">
-import { initRouter } from "@/router/utils";
-import { storageLocal } from "@pureadmin/utils";
-import { type CSSProperties, ref, computed } from "vue";
+import { computed, type CSSProperties, ref } from "vue";
 import { useUserStoreHook } from "@/store/modules/user";
-import { usePermissionStoreHook } from "@/store/modules/permission";
 
 defineOptions({
   name: "PermissionPage"
@@ -16,30 +13,7 @@ const elStyle = computed((): CSSProperties => {
   };
 });
 
-const username = ref(useUserStoreHook()?.username);
-
-const options = [
-  {
-    value: "admin",
-    label: "管理员角色"
-  },
-  {
-    value: "common",
-    label: "普通角色"
-  }
-];
-
-function onChange() {
-  useUserStoreHook()
-    .loginByUsername({ username: username.value, password: "admin123" })
-    .then(res => {
-      if (res.success) {
-        storageLocal().removeItem("async-routes");
-        usePermissionStoreHook().clearAllCachePage();
-        initRouter();
-      }
-    });
-}
+const roles = ref(useUserStoreHook()?.roles);
 </script>
 
 <template>
@@ -50,17 +24,9 @@ function onChange() {
     <el-card shadow="never" :style="elStyle">
       <template #header>
         <div class="card-header">
-          <span>当前角色：{{ username }}</span>
+          <span>当前角色：{{ roles }}</span>
         </div>
       </template>
-      <el-select v-model="username" class="!w-[160px]" @change="onChange">
-        <el-option
-          v-for="item in options"
-          :key="item.value"
-          :label="item.label"
-          :value="item.value"
-        />
-      </el-select>
     </el-card>
   </div>
 </template>
