d237ea9f8c14c2f3a09c2fb1ec7f97c1fec3fae8
Cade Li <cadecode@foxmail.com>
2024-06-09 18:12:03 +0800

    feat: welcome 页面添加用户名展示

--- src/views/welcome/index.vue
+++ src/views/welcome/index.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
+import { useUserStoreHook } from "@/store/modules/user";
+
 defineOptions({
   name: "Welcome"
 });
+
+const username = useUserStoreHook()?.username;
+const nickname = useUserStoreHook()?.nickname;
 </script>
 
 <template>
-  <h1>Pure-Admin-Thin（非国际化版本）</h1>
+  <div>
+    <el-card class="mb-4 box-card" shadow="never">
+      <span>欢迎登录！当前用户：{{ username }}（{{ nickname }}）</span>
+    </el-card>
+  </div>
 </template>
