3637317a8777900d05976ade34ded39ccf1b893f
Cade Li <cadecode@foxmail.com>
2024-06-10 10:26:33 +0800

    chore: 修改 LICENSE 文件及页面 Copyright

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -302,7 +302,7 @@ watch(checked, bool => {
     <div
       class="w-full flex-c absolute bottom-3 text-sm text-[rgba(0,0,0,0.6)] dark:text-[rgba(220,220,242,0.8)]"
     >
-      Copyright © 2020-present
+      Copyright © 2024
       <a
         class="hover:text-primary"
         href="https://github.com/pure-admin"
